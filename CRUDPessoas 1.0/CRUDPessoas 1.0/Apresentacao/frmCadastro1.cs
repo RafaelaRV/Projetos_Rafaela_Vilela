a870aee32a52f50fba1f9083a3d0f53016d88be9
RafaelaRV <142009217+RafaelaRV@users.noreply.github.com>
2023-09-18 15:58:50 -0300

    Primeiro Projeto utilizando CRUD
    Primeiro Projeto utilizando CRUD

---
+++ CRUDPessoas 1.0/CRUDPessoas 1.0/Apresentacao/frmCadastro1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CRUDPessoas_1._0
+{
+    internal class frmCadastro
+    {
+        internal void ShowDialog()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
